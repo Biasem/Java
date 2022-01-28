eafd3fb5daa4d36f7e606a0664c2518752f2fc9d
PC38\daw20 <rnietourena@safareyes.es>
2022-01-28 20:30:38 +0100

    fin v2

--- src/main/java/Utilidades/UtilidadesCliente.java
+++ src/main/java/Utilidades/UtilidadesCliente.java
@@ -5,11 +5,30 @@ import Modelos.Cliente;
 public class UtilidadesCliente {
 
     public boolean esDniValido(Cliente cliente){
+        int validar;
         String dni = cliente.getDni();
-        
-
-        return true;
+        String dni1 = dni.substring(0,8);
+        String dni2 = dni.substring(8);
+        if (dni.length()!= 9){
+            return false;
+        }
+        try{
+            validar = Integer.parseInt(dni1);
+        }catch (NumberFormatException e){
+            validar =-1;
+        }
+        if(validar==-1){
+            return false;
+        }
+        try{
+            validar = Integer.parseInt(dni2);
+        }catch (NumberFormatException e){
+            validar =-1;
+        }
+        if(validar>=0){
+            return true;
+        }else{
+            return false;
+        }
     }
-
-
 }
