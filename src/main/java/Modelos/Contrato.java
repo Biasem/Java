3a375775b7f9ccd0fc932a0cdc2014e38f16d8a4
PC38\daw20 <rnietourena@safareyes.es>
2022-02-01 19:39:37 +0100

    v3.1

--- src/main/java/Modelos/Contrato.java
+++ src/main/java/Modelos/Contrato.java
@@ -1,7 +1,68 @@
 package Modelos;
 
+import java.util.Objects;
+
 public class Contrato {
     private int identificador;
     private double salarioBase;
-    private TipoContrato tipoContrato
+    private TipoContrato tipoContrato;
+
+    public Contrato(int identificador,double salarioBase,TipoContrato tipoContrato){
+        this.identificador=identificador;
+        this.salarioBase = salarioBase;
+        this.tipoContrato=tipoContrato;
+    }
+    public Contrato(){}
+
+    public Contrato(Contrato contrato){
+        this.identificador= contrato.getIdentificador();
+        this.salarioBase = contrato.getSalarioBase();
+        this.tipoContrato= contrato.getTipoContrato();
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public TipoContrato getTipoContrato() {
+        return tipoContrato;
+    }
+
+    public void setTipoContrato(TipoContrato tipoContrato) {
+        this.tipoContrato = tipoContrato;
+    }
+
+    @Override
+    public String toString() {
+        return "Contrato{" +
+                "identificador=" + identificador +
+                ", salarioBase=" + salarioBase +
+                ", tipoContrato=" + tipoContrato +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contrato contrato = (Contrato) o;
+        return identificador == contrato.identificador && Double.compare(contrato.salarioBase, salarioBase) == 0 && tipoContrato == contrato.tipoContrato;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(identificador, salarioBase, tipoContrato);
+    }
 }
