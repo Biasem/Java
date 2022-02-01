6b5e0aa277593a1522b191c7577ec89184b07763
PC38\daw20 <rnietourena@safareyes.es>
2022-02-01 20:42:15 +0100

    v3.2 falta orden

--- src/main/java/Utilidades/UtilidadesEmpresa.java
+++ src/main/java/Utilidades/UtilidadesEmpresa.java
@@ -4,11 +4,31 @@ import Modelos.Empleado;
 import Modelos.Empresa;
 import Modelos.TipoContrato;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UtilidadesEmpresa {
     public static List<Empleado> getEmpleadosPorContrato(Empresa empresa, TipoContrato tipoContrato){
 
-        return empresa.getEmpleados().stream().filter(e-> e.getContrato().getTipoContrato().equals(tipoContrato));
+        return empresa.getEmpleados().stream().filter(e-> e.getContrato().getTipoContrato().equals(tipoContrato)).collect(Collectors.toList());
     }
+//falta ordenarlos
+    public static List<Empleado>getMileuristasOrdenadosPorSalario(Empresa empresa){
+
+        return empresa.getEmpleados().stream().filter(e->e.getContrato().getSalarioBase()>1000).collect(Collectors.toList());
+    }
+
+    public static double fondoSalarialEmpresa(Empresa empresa){
+
+        return empresa.getEmpleados().stream().mapToDouble(e->e.getContrato().getSalarioBase()).sum();
+    }
+
+    public Empleado getMejorPagado(List<Empresa> empresas){
+
+        for (empresas:Empresa){
+
+        }
+    }
+
 }
